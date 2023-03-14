29e68229b65795ae5849513c61d2b5adeca8099a
anazandi78 <anazandi78@gmail.com>
2023-03-14 15:43:08 -0700

    added func get_cid_from_db and made func get_blocks work with it

--- scripts/test.py
+++ scripts/test.py
@@ -65,10 +65,32 @@ def update_db_chain_head():
     mycursor.close()
     mydb.close()
 
+def get_cid_from_db():
+    mydb = get_database_connection()
+    mycursor = mydb.cursor()
+
+    select_query = "SELECT cid FROM cid"
+    mycursor.execute(select_query)
+
+    # Fetch all the rows of the query result
+    rows = mycursor.fetchall()
+
+    # Convert the rows to a list
+    cids = [row[0] for row in rows]
+
+    # Print the resulting list
+    # print(cids)
+
+    return cids
+
 def get_blocks(cids):
     results = []
+
+    formatted_cids = [{'/': item} for item in cids]
+    # print(formatted_cids)
+    
     # Loop through each cid and call the "Filecoin.ChainGetBlock" method
-    for cid in cids:
+    for cid in formatted_cids:
         data = {
             "id": 1,
             "jsonrpc": "2.0",
@@ -98,8 +120,10 @@ def get_blocks(cids):
 # print(chain_head)
 
 # update db with latest
-update_db_chain_head()
+# update_db_chain_head()
 
-# block_info = get_blocks(chain_head_cids)
-# print(json.dumps(block_info, indent=4))
-# print(f"The number of blocks: {len(block_info)}")
+cids = get_cid_from_db()
+block_info = get_blocks(cids)
+
+print(json.dumps(block_info, indent=4))
+print(f"The number of blocks: {len(block_info)}")
